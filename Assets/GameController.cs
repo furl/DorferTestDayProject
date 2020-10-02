9daa34eb1b083a0e2ec19aa0e7f260151fe4c3e4
furl <furl@bk.ru>
2020-10-02 16:57:59 +0300

    Добавил проценты
    На сколько процентов заполнен холст

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -8,14 +8,19 @@ public class GameController : MonoBehaviour {
 
     int levelNumber = 1;
     int likesNumber = 0;
+    int percentFilled = 0;
+
     public TMP_Text levelNumberText;
     public TMP_Text likesNumberText;
 
+
     public GameObject likesPanel;
     public GameObject tapToPlayPanel;
+    public GameObject percentagePanel;
     public GameObject levelNumberPanel;
     public GameObject restartPanel;
     public GameObject nextPanel;
+    
 
     public GameObject paintController;
 
@@ -28,6 +33,7 @@ public class GameController : MonoBehaviour {
     private void Awake() {
         likesPanel.SetActive(false);
         tapToPlayPanel.SetActive(true);
+        percentagePanel.SetActive(false);
         levelNumberPanel.SetActive(false);
         restartPanel.SetActive(false);
         nextPanel.SetActive(false);
@@ -44,11 +50,11 @@ public class GameController : MonoBehaviour {
         levelNumberText.text = "Level " + levelNumber.ToString();
         likesNumberText.text = likesNumber.ToString();
 
-
     }
 
     public void StartGame() {
         tapToPlayPanel.SetActive(false);
+        percentagePanel.SetActive(true);
         levelNumberPanel.SetActive(true);
         restartPanel.SetActive(true);
         likesPanel.SetActive(true);
